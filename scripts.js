70b0721ffe93f87cb8b9a5180c0e882be78006c8
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-05 16:52:34 +0200

    Converted  the sting  dividend and divider input into actual numbersr using the Number method

--- scripts.js
+++ scripts.js
@@ -1,21 +1,35 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+//const zerodDivider = document.querySelector("divider");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   const decRemovedResult = Math.trunc(dividend / divider); // went to mdn docs and learned that  Math.trunc can remove just the decimal without rounding
+  const conDivide = Number(dividend);
+  const conDivider = Number(divider);
+
+  if (!dividend) {
+    return (result.innerText =
+      "Division not performed. Both values are required in inputs. Try again."); // After the fact I learned that || but the truth is , I really dont understand it
+  }
+
+  if (!divider) {
+    return (result.innerText =
+      "Division not performed. Both values are required in inputs. Try again.");
+  }
+
+  //if (zerodDivider === "0") {
+  //return (result.innerText =
+  // "Division not performed. Invalid number provided. Try again");
+  //}
+  if (conDivider === 0) {
+    console.error("Division not performed. Invalid number provided.");
+    return (result.innerText =
+      "Division not performed. Invalid number provided. Try again.");
+  }
+
   result.innerText = decRemovedResult;
   result;
 });
-
-if (!dividend) {
-  return (result.innerText =
-    "Division not performed. Both values are required in inputs. Try again.");
-}
-
-if (!divider) {
-  return (result.innerText =
-    "Division not performed. Both values are required in inputs. Try again.");
-}
