cb33b4d4562a51e53ab3fef3a448c80400996a89
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-05 15:59:35 +0200

    Created two if statements that would log out  the error if nothing is put inside the boxes

--- scripts.js
+++ scripts.js
@@ -5,8 +5,17 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
   const decRemovedResult = Math.trunc(dividend / divider); // went to mdn docs and learned that  Math.trunc can remove just the decimal without rounding
-  result.innerText = truncatedResult;
+  result.innerText = decRemovedResult;
   result;
 });
+
+if (!dividend) {
+  return (result.innerText =
+    "Division not performed. Both values are required in inputs. Try again.");
+}
+
+if (!divider) {
+  return (result.innerText =
+    "Division not performed. Both values are required in inputs. Try again.");
+}
